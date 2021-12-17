38659c8de0d0f960cd46be6193ad4b0258ef7a94
guolh <guo_lh@mulangcloud.com>
2021-12-17 17:25:49 +0800

    fix bugs on file scripts/start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/bash
 
-workdir=$(cd $(dirname $0); pwd)
-source .venv/bin/activate
-pip3 install -r requirements.txt
+workdir=$(cd $(dirname $(dirname $0)); pwd)
+python3 -m venv ${workdir}/.venv --system-site-packages
+source ${workdir}/.venv/bin/activate
+pip3 install -r ${workdir}/requirements.txt
+if [ $? -ne 0 ]; then
+    echo 'pip failed'; exit
+fi
 
-python3 virtcdp/cmd/api.py --config-file etc/virtcdp.conf & 2>&1 /dev/null
-python3 virtcdp/cmd/agent.py --config-file etc/virtcdp.conf & 2>&1 /dev/null
+python3 ${workdir}/virtcdp/cmd/api.py --config-file ${workdir}/etc/virtcdp.conf & 2>&1 /dev/null
+if [ $? -ne 0 ]; then
+    echo 'Api service failed!'; exit
+fi
+
+python3 ${workdir}/virtcdp/cmd/agent.py --config-file ${workdir}/etc/virtcdp.conf & 2>&1 /dev/null
+if [ $? -ne 0 ]; then
+    echo 'Agent service failed!'; exit
+fi
